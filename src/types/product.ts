e729be02be1e058697e34af927615c2944007b5d
Kevin Carrera <kevin.cc@factory.generalsoftwareinc.com>
2025-01-14 12:33:28 -0500

    feat: migrate Next.js configuration to TypeScript and remove unused product type definition

--- src/types/product.ts
+++
@@ -1,8 +0,0 @@
-// export type Product = {
-//   id: number
-//   name: string
-//   description: string
-//   imageUrl: string
-//   price: string | number | null
-//   category: string | null
-// }
